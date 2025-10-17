8a2e503986217f0bf965654157b6204f66c0ca0b
Stanislav1s <stanislavstanislavov@Stanislavs-MacBook-Air.local>
2025-10-17 22:16:41 +0300

    add express to api

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,10 @@
-console.log('works')
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log('it works');
+    res.send('it works')
+
+})
+app.listen(3030, () => 'server is listening on http://localhost:3030...')
