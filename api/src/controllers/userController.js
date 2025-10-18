b1ffb075c4b530bcfc45e1c6b74d58d4b2915773
Stanislav1s <stanislavstanislavov@Stanislavs-MacBook-Air.local>
2025-10-18 13:57:49 +0300

    Return token after login

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -10,5 +10,12 @@ userController.post('/register', async (req, res) => {
 })
 userController.post('/login', async (req, res) => {
     const { email, password } = req.body
+    try {
+        const result = await userService.login(email, password)
+        res.status(201).json(result)
+    } catch (err) {
+        res.status(401).json({ message: err.message });
+
+    }
 })
 export default userController
--- api/src/services/userService.js
+++ api/src/services/userService.js
@@ -1,9 +1,10 @@
-import user from "../models/User.js"
+import User from "../models/User.js"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 export default {
     register(email, password) {
-        return user.create({ email, password })
+        return User.create({ email, password })
     },
     async login(email, password) {
         const user = await User.findOne({ email })
@@ -11,10 +12,21 @@ export default {
             throw new Error('Invalid email or password')
         }
         const isValid = await bcrypt.compare(password, user.password);
-        if (isValid) {
+        if (!isValid) {
             throw new Error('Invalid email or password')
         }
-        return user
+
+        const payload = {
+            id: user.id,
+            email: user.email
+        }
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '2h' })
+        return {
+            _id: user.id,
+            email: user.email,
+            accessToken: token
+        }
     }
 
 }
