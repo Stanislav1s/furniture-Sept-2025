633a3637f6ca764441126a1035940ab8fe080587
Stanislav1s <stanislavstanislavov@Stanislavs-MacBook-Air.local>
2025-10-18 12:07:22 +0300

    Add mongoose

--- api/src/index.js
+++ api/src/index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import cors from "cors"
 import routes from "./routes.js";
+import mongoose from "mongoose"
 
 const app = express();
+try {
+    await mongoose.connect('mongodb://localhost:27017/', {
+        dbName: "furnitureBackend"
+    });
+    console.log('Connected to the DB')
+} catch (err) {
+    console.error('Cannot connect to the DB!')
+    console.error(err.message)
+}
 
 app.use(cors());
 app.use(express.json())
