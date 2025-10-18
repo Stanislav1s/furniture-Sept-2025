18f66422a19fa09a3be4a6cce6a95ab797f97f06
Stanislav1s <stanislavstanislavov@Stanislavs-MacBook-Air.local>
2025-10-18 11:02:50 +0300

    add json parser

--- api/src/index.js
+++ api/src/index.js
@@ -5,6 +5,7 @@ import routes from "./routes.js";
 const app = express();
 
 app.use(cors());
+app.use(express.json())
 
 app.use(routes);
 app.listen(3030, () => 'server is listening on http://localhost:3030...')
